a81f6f2a00e8a6c8c61d4aacf52549904d7a5ec0
maxam2017 <whk0313@gmail.com>
2022-10-02 15:08:46 +0800

    chore: add more info for install sh

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-sudo -v
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+gh-ssh-keygen -h &> /dev/null
+if [ $? -ne 0 ];then
+    sudo -v
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo curl -fsSL -o /usr/local/bin/gh-ssh-keygen https://raw.githubusercontent.com/maxam2017/gh-ssh-keygen/HEAD/gh-ssh-keygen.sh
-sudo chmod +x /usr/local/bin/gh-ssh-keygen
+    sudo curl -fsSL -o /usr/local/bin/gh-ssh-keygen https://raw.githubusercontent.com/maxam2017/gh-ssh-keygen/HEAD/gh-ssh-keygen.sh
+    sudo chmod +x /usr/local/bin/gh-ssh-keygen
+
+    echo "✅ Installation successful!"
+    echo
+    gh-ssh-keygen -h
+    echo
+else
+    echo -e "\x1b[2mgh-ssh-keygen is already installed."
+fi
